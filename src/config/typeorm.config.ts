3af616e273005b0a0aeccefa1ad0ee88f7753860
Dmitry.Polyakov <Polyakov.dima111@gmail.com>
2021-04-12 21:25:54 +0300

    added postgreSQL and typeORM

---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,20 @@
+require('dotenv').config();
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const typeOrmConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+  synchronize: true,
+  // for remote the DB
+  ssl: true,
+  extra: {
+  ssl: {
+    rejectUnauthorized: false,
+  },
+},
+}
